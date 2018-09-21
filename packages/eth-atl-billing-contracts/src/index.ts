baeb3289e4221490ef5b359f97847f5408d8b2e5
Micah Riggan <mriggan@bitpay.com>
2018-09-21 19:21:33 -0400

    Wallet creation works

---
+++ packages/eth-atl-billing-contracts/src/index.ts
@@ -0,0 +1,11 @@
+export const Contracts = {
+  BillableWalletFactory: {
+    spec: require("../blockchain/build/contracts/BillableWalletFactory.json"),
+    address: "0xf5cd081e313df55903ba6495ca24e31b5fed049f"
+  },
+  BillableWallet: {
+    spec: require("../blockchain/build/contracts/BillableWallet.json"),
+    address: ""
+  }
+};
+
--- packages/eth-atl-billing-frontend-react/src/containers/main.tsx
+++ packages/eth-atl-billing-frontend-react/src/containers/main.tsx
@@ -1,12 +1,26 @@
-import * as React from 'react';
+import * as React from "react";
+import { Web3Component } from "../components/Web3Component";
 
+export class MainPage extends Web3Component {
+  constructor(props: any) {
+    super(props);
+    this.createWallet = this.createWallet.bind(this);
+  }
 
-export class MainPage extends React.Component {
+  public async createWallet() {
+    const accounts = await this.getAccounts();
+    const account = accounts[0];
+    const tx = await this.getWalletFactory()
+      .methods.createWallet()
+      .send({from: account});
+    window.console.log(tx);
+  }
 
   public render() {
     return (
-      <div>Main Page </div>
+      <div>
+        <button onClick={this.createWallet}>Create Wallet </button>
+      </div>
     );
   }
 }
-
