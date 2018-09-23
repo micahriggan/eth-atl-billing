b89ebbfce177a65cd34b0a803ac8370a8343f1f1
Micah Riggan <mriggan@bitpay.com>
2018-09-23 01:05:40 -0400

    Wiring up dropdown to authorize ui

--- packages/eth-atl-billing-frontend-react/src/components/Recurrence/Recurrence.tsx
+++ packages/eth-atl-billing-frontend-react/src/components/Recurrence/Recurrence.tsx
@@ -1,99 +1,113 @@
 import * as React from "react";
-import {Dropdown} from "semantic-ui-react";
+import { Dropdown } from "semantic-ui-react";
+import { DropdownProps } from "semantic-ui-react/dist/commonjs/modules/Dropdown/Dropdown";
 
 interface IState {
-    selectedRecurrence: string;
+  selectedRecurrence: string;
 }
 
 interface IProps {
-    onChange: (data: any) => void;
+  onChange: (data: string) => void;
+}
+export enum TIMES {
+  ANNUAL = "31557600000",
+  MONTH = "2629800000",
+  BIWEEK = "1209600000",
+  WEEK = "604800000",
+  DAY = "86400000",
+  HOUR = "3600000"
 }
 
 export class Recurrence extends React.Component<IProps, IState> {
-    public state: IState = {
-        selectedRecurrence: "Monthly"
-    };
+  public state: IState = {
+    selectedRecurrence: TIMES.ANNUAL
+  };
 
-    public render() {
-        const recurrenceSelected = this.state.selectedRecurrence;
-        return this.dropdownRecurrence(recurrenceSelected);
+  public render() {
+    const recurrenceSelected = this.state.selectedRecurrence;
+    return this.dropdownRecurrence(recurrenceSelected);
+  }
+
+  public onChange = (event: React.SyntheticEvent<HTMLElement>, data: DropdownProps) => {
+    if (data.value) {
+      this.setState({ selectedRecurrence: data.value.toString() });
+      this.props.onChange(data.value.toString());
     }
+  };
 
-    public onChange = (data: any) => {
-        this.setState({ selectedRecurrence: data.value});
-        this.props.onChange(data.value);
-    }
+  public dropdownRecurrence(recurrenceSelected: string) {
+    const recurrenceOptions = [
+      {
+        text: "Annually",
+        value: TIMES.ANNUAL,
+        image: {
+          avatar: true,
+          src: "../../../../images/recurrenceVisuals/icons8-a-26.png",
+          style: { height: "18px", verticalAlign: "middle" },
+          className: "no-border-radius"
+        }
+      },
+      {
+        text: "Monthly",
+        value: TIMES.MONTH,
+        image: {
+          avatar: true,
+          src: "../../../../images/recurrenceVisuals/icons8-m-26.png",
+          style: { height: "18px", verticalAlign: "middle" },
+          className: "no-border-radius"
+        }
+      },
+      {
+        text: "Biweekly",
+        value: TIMES.BIWEEK,
+        image: {
+          avatar: true,
+          src: "../../../../images/recurrenceVisuals/icons8-b-26.png",
+          style: { height: "18px", verticalAlign: "middle" },
+          className: "no-border-radius"
+        }
+      },
+      {
+        text: "Weekly",
+        value: TIMES.WEEK,
+        image: {
+          avatar: true,
+          src: "../../../../images/recurrenceVisuals/icons8-w-26.png",
+          style: { height: "18px", verticalAlign: "middle" },
+          className: "no-border-radius"
+        }
+      },
+      {
+        text: "Daily",
+        value: TIMES.DAY,
+        image: {
+          avatar: true,
+          src: "../../../../images/recurrenceVisuals/icons8-d-26.png",
+          style: { height: "18px", verticalAlign: "middle" },
+          className: "no-border-radius"
+        }
+      },
+      {
+        text: "Hourly",
+        value: TIMES.HOUR,
+        image: {
+          avatar: true,
+          src: "../../../../images/recurrenceVisuals/icons8-h-26.png",
+          style: { height: "18px", verticalAlign: "middle" },
+          className: "no-border-radius"
+        }
+      }
+    ];
 
-    public dropdownRecurrence(recurrenceSelected: string) {
-        const recurrenceOptions = [
-            {
-                text: 'Annually',
-                value: '31557600000',
-                image: {
-                    avatar: true,
-                    src: '../../../../images/recurrenceVisuals/icons8-a-26.png',
-                    style: {height: "18px", verticalAlign: "middle"},
-                    className: "no-border-radius"
-                }
-            },
-            {
-                text: 'Monthly',
-                value: '2629800000',
-                image: {
-                    avatar: true,
-                    src: '../../../../images/recurrenceVisuals/icons8-m-26.png',
-                    style: {height: "18px", verticalAlign: "middle"},
-                    className: "no-border-radius"
-                }
-            },
-            {
-                text: 'Biweekly',
-                value: '1209600000',
-                image: {
-                    avatar: true,
-                    src: '../../../../images/recurrenceVisuals/icons8-b-26.png',
-                    style: {height: "18px", verticalAlign: "middle"},
-                    className: "no-border-radius"
-                }
-            },
-            {
-                text: 'Weekly',
-                value: '604800000',
-                image: {
-                    avatar: true,
-                    src: '../../../../images/recurrenceVisuals/icons8-w-26.png',
-                    style: {height: "18px", verticalAlign: "middle"},
-                    className: "no-border-radius"
-                }
-            },
-            {
-                text: 'Daily',
-                value: '86400000',
-                image: {
-                    avatar: true,
-                    src: '../../../../images/recurrenceVisuals/icons8-d-26.png',
-                    style: {height: "18px", verticalAlign: "middle"},
-                    className: "no-border-radius"
-                }
-            },
-            {
-                text: 'Hourly',
-                value: '3600000',
-                image: {
-                    avatar: true,
-                    src: '../../../../images/recurrenceVisuals/icons8-h-26.png',
-                    style: {height: "18px", verticalAlign: "middle"},
-                    className: "no-border-radius"
-                }
-            },
-        ];
-
-        return (<Dropdown placeholder='Select payment frequency'
-                          fluid={true}
-                          selection={true}
-                          defaultSelectedLabel={this.state.selectedRecurrence}
-                          options={recurrenceOptions}
-                          defaultValue={recurrenceSelected}
-                          onChange={this.onChange}/>);
-    }
+    return (
+      <Dropdown
+        placeholder="Select payment frequency"
+        fluid={true}
+        selection={true}
+        options={recurrenceOptions}
+        defaultValue={recurrenceSelected}
+        onChange={this.onChange}
+      />
+    );
+  }
 }
